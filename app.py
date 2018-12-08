6b92be591e61a819d7d305ca62ff7b7736847e8e
diW <di.W@hotmail.com>
2018-12-09 00:13:43 +0800

    add getting wav using html

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
-
+import os
 #app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 # configuration
@@ -16,7 +16,6 @@ PASSWORD = 'default'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
@@ -81,6 +80,16 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('show_entries'))
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/get_audio', methods=['GET', 'POST'])
+def get_audio():
+    if request.method == 'POST':
+        request.files['file'].save('a.wav')
+
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run()
 
